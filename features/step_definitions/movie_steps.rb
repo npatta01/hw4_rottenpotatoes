b581d7d302b6e4b101a05542f85bec6c112f2715
Nidhin Pattaniyil <npatta01@gmail.com>
2013-08-06 21:04:10 -0400

    Finished Scenario 1

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,17 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    @movie = Movie.create!(movie)
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  end
+  #flunk "Unimplemented"
+end
+
+
+Then(/^the director of "(.*?)" should be "(.*?)"$/) do |title, director|
+  @movie = Movie.find_by_title(title)
+  page.should have_content(director)
+  @movie.director.should ==director
+end
